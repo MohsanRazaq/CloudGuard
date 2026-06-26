f0aa5a871d0846adec3393a7568b4fb5ec24d950
MohsanRazaq <mohsansec@gmail.com>
2026-06-27 00:40:57 +0500

    feat: integrate AWS IAM authentication and S3 scanning

--- cloudguard/aws/session.py
+++ cloudguard/aws/session.py
@@ -1,11 +1,9 @@
 import boto3
 
 
-def create_session():
+def create_session(region="us-east-1"):
     return boto3.client(
         "s3",
-        endpoint_url="http://localhost:4566",
-        aws_access_key_id="test",
-        aws_secret_access_key="test",
-        region_name="us-east-1"
+        region_name=region
     )
+    
