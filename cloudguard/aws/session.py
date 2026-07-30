886c04b898f40bedfc305fd1c912b24590b0eef5
MohsanRazaq <mohsansec@gmail.com>
2026-07-30 08:17:52 +0500

    feat: Merged Session  for Production and testing

--- cloudguard/aws/session.py
+++ cloudguard/aws/session.py
@@ -1,23 +1,7 @@
-import boto3
-import run_mock_aws
+# import boto3
+# uncomment when  you want to interact with real aws .for testing you can use this local stack wrapper
+from aws_wrapper import get_boto3_session
+session=get_boto3_session()
 def create_session(region="us-east-1"):
-    return boto3.Session(
-        region_name=region
-    )
+    return session
     
-    
-# 1. Initialize your session
-session = create_session()
-
-# 2. Generate a client (e.g., IAM)
-client = session.client('iam')
-
-# 3. Inspect the hidden meta endpoint URL
-active_endpoint = client.meta.endpoint_url
-print(f"\n🔍 Currently connected to: {active_endpoint}")
-
-# 4. Print an explicit warning status
-if "localhost" in active_endpoint or "127.0.0.1" in active_endpoint:
-    print("⚠️  STATUS: SAFE! You are running on MOCK AWS (Moto). No real charges.")
-else:
-    print("🚨 STATUS: LIVE PRODUCTION! You are hitting REAL AWS servers.")
